d0a03ad13f680bb8155ede9287fc5ccc9411f7cd
fuad7us <fuad7us@yahoo.com>
2018-03-12 22:36:49 +0600

    class-31

---
+++ class-31/assets/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){        
+        var s = skrollr.init({
+          render:function(data){
+            
+          }
+        })   
+
+
+    });
+
+
+    jQuery(window).load(function(){
+    	
+        
+    });
+
+
+}(jQuery));	
